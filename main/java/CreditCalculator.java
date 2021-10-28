c2103119d21660a6a4a67651aba2815f68df6ac5
Danil Agafonov <javaDanilAgafonov@yandex.ru>
2021-10-28 20:13:53 +0300

    Написаны тесты

--- main/java/CreditCalculator.java
+++ main/java/CreditCalculator.java
@@ -13,17 +13,28 @@ public class CreditCalculator {
 	}
 	
 	public double monthlyPayment() {
-		return 0;
+		double annuityRatio = (monthlyInterestRate() * Math.pow((1 + monthlyInterestRate()), month)) /
+				(Math.pow((1 + monthlyInterestRate()), month) - 1);
+		return formatDouble(loanAmount * annuityRatio);
 	}
 	
 	public double totalLoanAmount() {
 		
-		return 0;
+		return monthlyPayment() * month;
 	}
 	
 	public double overpayment() {
-		return 0;
+		return formatDouble(totalLoanAmount() - loanAmount);
 	}
 	
-
+	private double formatDouble(double value) {
+		DecimalFormat decimalFormat = new DecimalFormat("#.##");
+		String strRes = decimalFormat.format(value);
+		String strRes2 = strRes.replace(",", ".");
+		return Double.parseDouble(strRes2);
+	}
+	
+	private double monthlyInterestRate() {
+		return percent / 12 / 100;
+	}
 }
